ba9b74c226b9d4ba3173fb5ada22433431f6e9f0
Filipi Andrade Rocha <filipiandrader@gmail.com>
2022-01-25 19:27:57 -0300

    Setup gradle deps

--- app/src/main/java/com/far/notesapp/MainActivity.kt
+++ app/src/main/java/com/far/notesapp/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NotesAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Greeting("Android")
                 }
@@ -32,7 +31,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    NotesAppTheme {
+    NotesAppTheme() {
         Greeting("Android")
     }
 }
